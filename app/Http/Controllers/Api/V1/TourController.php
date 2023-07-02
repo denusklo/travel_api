1fd92c90b061747ec0664667bbb04e97b089b108
unknown <kong@lekir.tech>
2023-07-03 02:12:13 +0800

    tour and travel test. create user

---
+++ app/Http/Controllers/Api/V1/TourController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Travel;
+use App\Http\Resources\TourResource;
+use Illuminate\Http\Request;
+use App\Http\Requests\ToursListRequest;
+
+class TourController extends Controller
+{
+    public function index(Travel $travel, ToursListRequest $request)
+    {
+        $tours = $travel->tours() // Tour::where('travel_id', $travel->id)
+            ->when($request->priceFrom, function ($query) use ($request) {
+                $query->where('price', '>=', $request->priceFrom * 100);
+            })
+            ->when($request->priceTo, function ($query) use ($request) {
+                $query->where('price', '<=', $request->priceTo * 100);
+            })
+            ->when($request->dateFrom, function ($query) use ($request) {
+                $query->where('starting_date', '>=', $request->dateFrom);
+            })
+            ->when($request->dateTo, function ($query) use ($request) {
+                $query->where('starting_date', '<=', $request->dateTo);
+            })
+            ->when($request->sortBy && $request->sortOrder, function ($query) use ($request) {
+                $query->orderBy($request->sortBy, $request->sortOrder);
+            })
+            ->orderBy('starting_date')
+            ->paginate();
+
+        return TourResource::collection($tours);
+    }
+}
